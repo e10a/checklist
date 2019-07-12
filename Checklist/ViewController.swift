b336764a866e801e0d4473895cdd5a00195caa3f
Ellen Shimada <ellenshimada@gmail.com>
2019-07-12 15:36:06 -0700

    Initial Commit

---
+++ Checklist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by e10a on 7/12/19.
+//  Copyright © 2019 e10a. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
