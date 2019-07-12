c81fc61b71de7f22d1361b46c92164d9fc4bb4a4
Ellen Shimada <ellenshimada@gmail.com>
2019-07-12 16:19:17 -0700

    Display placeholder data

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ChecklistViewController
 //  Checklist
 //
 //  Created by e10a on 7/12/19.
@@ -15,6 +15,30 @@ class ChecklistViewController: UITableViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1000
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        if let label = cell.viewWithTag(1000) as? UILabel {
+            switch indexPath.row % 5 {
+            case 0:
+                label.text = "Take a jog"
+            case 1:
+                label.text = "Watch a movie"
+            case 2:
+                label.text = "Code an app"
+            case 3:
+                label.text = "Walk the dog"
+            case 4:
+                label.text = "Study design patterns"
+            default:
+                break;
+            }
+        }
+        return cell
+    }
 
 }
 
